acfa13f97914066ea34aa6f4a002ddb4e5ecd6a8
Ranjan <ranjanpandey2006@gmail.com>
2024-01-26 17:55:07 +0530

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CreateItem from './components/pages/CreateItem';
+import UpdateItem from './components/pages/UpdateItem';
+import ItemsOverview from './components/pages/ItemsOverview';
+import ItemDetail from './components/pages/ItemDetail';
+
+axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: ItemsOverview },
+        { path: '/create', component: CreateItem },
+        { path: '/edit/:id', component: UpdateItem },
+        { path: '/detail/:id', component: ItemDetail }
+    ]
+});
+
+createApp(App).use(router).mount('#app')
